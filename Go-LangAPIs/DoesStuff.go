2bb6d7f55ee01fe7f34e7f3e5d4bf8ab4f7a82b9
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2021-08-13 17:30:11 +0530

    Works with DB now. APIs are the same, data is being flushed to a DB at the back.

--- Go-LangAPIs/DoesStuff.go
+++ Go-LangAPIs/DoesStuff.go
@@ -2,42 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-
 type Note struct {
-	Key     int64  `json:"key"`
+	ID      int64  `json:"key"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
 
 // This houses all the notes hardwired
-var noteList = []Note{
-	{
-		Key:     1,
-		Title:   "Delegation",
-		Content: "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem",
-	},
-	{
-		Key:     2,
-		Title:   "Loops",
-		Content: "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat.",
-	},
-	{
-		Key:     3,
-		Title:   "Arrays",
-		Content: "Q. Why did the programmer quit his job? A. Because he didn't get arrays.",
-	},
-	{
-		Key:     4,
-		Title:   "Hardware vs. Software",
-		Content: "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
-	},
+
+
+var dsn = "host=localhost user=jawad_notesapp database=mydb password= dbname=mydb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+var db *gorm.DB
+var DbEr error
+
+func MakeCon() (*gorm.DB, error) {
+	db, DbErrObj := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return db, DbErrObj
 }
 
 func jsonWrapper(next http.Handler) http.Handler {
@@ -51,16 +42,25 @@ func jsonWrapper(next http.Handler) http.Handler {
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "You're on the homepage")
 }
-func fetchAllTasks(w http.ResponseWriter, r *http.Request) {
-	marshalledTask, err := json.Marshal(noteList)
-	if err != nil {
-		fmt.Fprint(w, "There was an error")
+
+func getNotes(w http.ResponseWriter, r *http.Request) {
+
+	var AllNotes = make([]Note, 0)
+	res := db.Find(&AllNotes)
+	if res.Error != nil {
+		log.Fatal("There was an error", res)
 	} else {
-		w.Write(marshalledTask)
+		marshalledTask, err := json.Marshal(AllNotes)
+		if err != nil {
+			fmt.Fprint(w, "There was an error", err)
+		} else {
+			w.Write(marshalledTask)
+		}
 	}
 }
+
 func pageNotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Add("Content-Type", "text/plain")
+
 	w.WriteHeader(http.StatusNotFound)
 	t := struct {
 		Msg string `json:"message"`
@@ -68,28 +68,26 @@ func pageNotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	msg, _ := json.Marshal(t)
 	w.Write(msg)
 }
-func fetchTaskById(w http.ResponseWriter, r *http.Request) {
+
+func getNoteByID(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.ParseInt(mux.Vars(r)["id"], 10, 64)
-
-	for _, noteItem := range noteList {
-		if noteItem.Key == id {
-
-			marshalledTask, err := json.Marshal(noteItem)
-			if err != nil {
-				fmt.Fprint(w, "There was an error")
-			} else {
-				w.Write(marshalledTask)
-			}
-
-			return
+	var noteItem Note
+	result := db.First(&noteItem, id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		pageNotFoundHandler(w, r)
+	} else {
+		marshalledTask, err := json.Marshal(noteItem)
+		if err != nil {
+			fmt.Fprint(w, "There was an error")
+		} else {
+			w.Write(marshalledTask)
 		}
 
 	}
-	pageNotFoundHandler(w, r)
-
 }
+
 func errHandler(w http.ResponseWriter, err error) {
-	// w.Header().Add("Content-Type", "text/plain")
+
 	ms := fmt.Sprintln(err)
 	t := struct {
 		Msg string `json:"message"`
@@ -100,16 +98,15 @@ func errHandler(w http.ResponseWriter, err error) {
 
 }
 
-func addTask(w http.ResponseWriter, r *http.Request) {
+func addNoteToDB(w http.ResponseWriter, r *http.Request) {
 	item, err := decodeAndRetBody(r)
 	if err != nil {
 		errHandler(w, err)
 		return
 	}
-	noteList = append(noteList, item)
-	w.WriteHeader(http.StatusCreated)
-	// fmt.Println(item)
 
+	db.Select("title", "content").Create(&item)
+	w.WriteHeader(http.StatusCreated)
 }
 func decodeAndRetBody(r *http.Request) (Note, error) {
 	d := json.NewDecoder(r.Body)
@@ -119,59 +116,62 @@ func decodeAndRetBody(r *http.Request) (Note, error) {
 	return item, err
 
 }
-func modifyTask(w http.ResponseWriter, r *http.Request) {
+
+func updateNote(w http.ResponseWriter, r *http.Request) {
+	// DB call find by id
 	id, _ := strconv.ParseInt(mux.Vars(r)["id"], 10, 64)
+	var noteItem Note
+	result := db.First(&noteItem, id)
+	statResp := http.StatusOK
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		statResp = http.StatusCreated
+	}
 	updateItem, err := decodeAndRetBody(r)
+	updateItem.ID = id
 	if err != nil {
 		errHandler(w, err)
 		return
 	}
-	for index, noteItem := range noteList {
-		if noteItem.Key == id {
-			if updateItem.Title != "" {
-				noteList[index].Title = updateItem.Title
-			}
-			if updateItem.Content != "" {
-				noteList[index].Content = updateItem.Content
-			}
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+	result = db.Save(&updateItem)
+	if result.Error != nil {
+		log.Println("Error when saving:", result.Error)
+		errHandler(w, result.Error)
+		return
 	}
-	noteList = append(noteList, updateItem)
-	w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(statResp)
 }
 
-func removeNoteFromList(i int) {
-	noteList[i] = noteList[len(noteList)-1]
-	noteList = noteList[:len(noteList)-1]
-}
-
-func removeTask(w http.ResponseWriter, r *http.Request) {
+func deleteNote(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.ParseInt(mux.Vars(r)["id"], 10, 64)
-	for index, noteItem := range noteList {
-		if noteItem.Key == id {
-			removeNoteFromList(index)
-			w.WriteHeader(http.StatusNoContent)
-		}
+	var noteItem Note
+	result := db.Delete(&noteItem, id)
+	if result.RowsAffected  == 0 {
+		pageNotFoundHandler(w, r)
+		return
 	}
-	pageNotFoundHandler(w, r)
+	
+	w.WriteHeader(http.StatusNoContent)
 
 }
 
 func main() {
+	db, DbEr = MakeCon()
+	if DbEr != nil {
+		log.Fatal("Database failed to connect", DbEr)
+	}
+
 	homeRouter := mux.NewRouter()
 	homeRouter.HandleFunc("/", homeHandler)
 
 	noteRouter := homeRouter.PathPrefix("/api/").Subrouter()
 	noteRouter.Use(jsonWrapper)
 
-	noteRouter.HandleFunc("/task/", fetchAllTasks).Methods("GET")
-	noteRouter.HandleFunc("/task/", addTask).Methods("POST")
+	noteRouter.HandleFunc("/task/", getNotes).Methods("GET")     //changed
+	noteRouter.HandleFunc("/task/", addNoteToDB).Methods("POST") //changed
 
-	noteRouter.HandleFunc("/task/{id}", fetchTaskById).Methods("GET")
-	noteRouter.HandleFunc("/task/{id}", modifyTask).Methods("PUT")
-	noteRouter.HandleFunc("/task/{id}", removeTask).Methods("DELETE")
+	noteRouter.HandleFunc("/task/{id}", getNoteByID).Methods("GET") //changed
+	noteRouter.HandleFunc("/task/{id}", updateNote).Methods("PUT")  //changed
+	noteRouter.HandleFunc("/task/{id}", deleteNote).Methods("DELETE") //changed
 
 	http.Handle("/", homeRouter)
 	log.Fatal(http.ListenAndServe(":8000", nil))
